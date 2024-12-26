dbdffab984912ecedbafaa6a70e7f66e2bac87b0
AkramAboJbara <akramabojbara@gmail.com>
2024-12-26 15:42:19 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path,include
 from .views import CategoryListAPIView, HomepageProductAPIView, homepage
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import viewsets_product
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('product', viewsets_product)
+
 
 urlpatterns = [
     path('', homepage, name='homepage'),
     path('home/products/', HomepageProductAPIView.as_view(), name='homepage-products'),
     path('categories/', CategoryListAPIView.as_view(), name='category-list'),
+    path('product/', include(router.urls)),
 ]
