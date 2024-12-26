2ee0f7da0fc3ce2437cad7baf127166b768daeb0
AkramAboJbara <akramabojbara@gmail.com>
2024-12-26 15:42:22 +0200

    Update serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
-from .models import User, Product, Cart, ShoppingCart, Order, OrderItem, Category
+from .models import User, Product, Cart, CartItem, Order, OrderItem, Category
 from django.contrib.auth import authenticate
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=8)
     class Meta:
         model = User
         fields = ['email', 'password', 'phone_number', 'address', 'first_name', 'last_name', 'is_active']
         extra_kwargs = {'password': {'write_only': True}}
 
+    def validate_password(self, value):
+        # Add more custom password validation here if necessary
+        if len(value) < 8:
+            raise ValidationError("Password must be at least 8 characters long.")
+        if not any(char.isdigit() for char in value):
+            raise ValidationError("Password must contain at least one digit.")
+        if not any(char.isalpha() for char in value):
+            raise ValidationError("Password must contain at least one letter.")
+        return value
+    
     def create(self, validated_data):
         user = User(
             email=validated_data['email'],
@@ -21,18 +32,24 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 class ProductSerializer(serializers.ModelSerializer):
+    price_after_discount = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_price_after_discount(self, obj):
+        discount_amount = obj.price * (obj.discount_percentage / 100)
+        return obj.price - discount_amount
 
 class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields =  ['id','quantity']
 
-class ShoppingCartSerializer(serializers.ModelSerializer):
+class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ShoppingCart
+        model = CartItem
         fields = '__all__'
 
 class OrderSerializer(serializers.ModelSerializer):
