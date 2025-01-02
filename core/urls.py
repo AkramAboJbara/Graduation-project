776c8abd118f7f586068cf538cc1a3e3d67bdd8d
AkramAboJbara <akramabojbara@gmail.com>
2025-01-02 12:18:26 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path,include
-from .views import CategoryListAPIView, HomepageProductAPIView, homepage
+from .views import HomepageProductAPIView, homepage
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import viewsets_product
+from .views import viewsets_product,viewsets_category
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('product', viewsets_product)
+router.register('category', viewsets_category)
 
 
 urlpatterns = [
     path('', homepage, name='homepage'),
     path('home/products/', HomepageProductAPIView.as_view(), name='homepage-products'),
-    path('categories/', CategoryListAPIView.as_view(), name='category-list'),
+    path('category/', include(router.urls)),
     path('product/', include(router.urls)),
 ]
