1c7354257ef9c6b57a68352aa634950a935d2dc3
abdelrhman Beseiso <138146031+abdelrhman1beseiso@users.noreply.github.com>
2025-06-07 20:11:30 +0300

    Update models.py

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,11 +1,10 @@
-# models.py
 from django.db import models
 from django.contrib.auth.models import User
 
 class Review(models.Model):
     email = models.EmailField()
     message = models.TextField()
-    rating = models.PositiveIntegerField(default=1)  # 1 to 5 stars
+    rating = models.PositiveIntegerField(default=1)  
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
